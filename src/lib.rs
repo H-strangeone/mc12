8d8ac366d6b05b0183a191bdae1633f8db2627c7
H-strangeone <hiteshgitman@gmail.com>
2024-08-07 01:51:27 -0700

    i

---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn parse() {
+//         assert_eq!(Number::new("123"),Number(123));
+//     }
+// }
+
+// #[derive(Debug, PartialEq)]
+
+#[cfg(test)]
+mode tests {
+    use super::*;
+
+    #[test]
+    fn parse(){
+        assert_eq!(Number::new("123"),Number(123)):
+    }
+}
+
